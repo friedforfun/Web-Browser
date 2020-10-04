6b726e48bc9fc9a807fa30e0b6a5e2120f582447
Sam <samfayhunt@gmail.com>
2020-10-04 01:32:11 +0100

    Redesign HttpRequest to return browser response object

--- Web-Browser/HttpRequests.cs
+++ Web-Browser/HttpRequests.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Net.Http;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
 
 namespace Web_Browser
 {
@@ -20,27 +23,45 @@ namespace Web_Browser
             client = new HttpClient();
         }
 
-        public static async Task Get(string uri)
+        public static async Task<BrowserResponse> Get(string uri)
         {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(uri);
-
-                // possibly change to handle non-success status code
-                response.EnsureSuccessStatusCode();
-
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-                Console.WriteLine(response.StatusCode);
-                Console.WriteLine(responseBody);
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("Exception");
-                Console.WriteLine("Message: {0}", e.Message);
-            }
-            
+            var httpres = await client.GetAsync(uri);
+            return new BrowserResponse(httpres);
         }
 
     }
+
+    public class BrowserResponse
+    {
+        public string Title { get => _title; }
+        private string _title;
+        public string HttpSourceCode { get => _source; }
+        private string _source;
+        public string Url { get => _url; }
+        private string _url;
+        public int StatusCode { get; }
+        private int _statusCode;
+
+        private IDocument _document;
+        private string _rawContent;
+
+        public BrowserResponse(HttpResponseMessage message)
+        {
+            GetContent(message.Content);
+            _statusCode = (int)message.StatusCode;
+        }
+
+        private async void GetContent(HttpContent content)
+        {
+            IConfiguration config = Configuration.Default;
+            IBrowsingContext context = BrowsingContext.New(config);
+
+            _rawContent = await content.ReadAsStringAsync();
+            _document = await context.OpenAsync(req => req.Content(_rawContent));
+            _title = _document.Title;
+            _url = _document.Url;
+            _source = _document.Source.Text;
+        }
+    }
+
 }
